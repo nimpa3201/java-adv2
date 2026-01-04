528e1118174e23174fcdcdffa6f346c6c1de398a
gwongilnam <nimpa3201@naver.com>
2026-01-05 07:14:19 +0900

    feat: 네트워크 예외2 - 타임아웃

---
+++ src/network/exception/connect/SoTimeoutServer.java
@@ -0,0 +1,15 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class SoTimeoutServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket(12345);
+        serverSocket.accept();
+
+        Thread.sleep(1000000); // 무한 슬립
+
+    }
+}
